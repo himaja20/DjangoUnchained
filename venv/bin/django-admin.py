d9c80b4db96e031ffeabd94a3164e674c7ce3a56
Himaja <himaja.auce@gmail.com>
2016-01-02 19:14:00 -0800

    gitignore

--- venv/bin/django-admin.py
+++
@@ -1,4 +0,0 @@
-#!/home/himaja/workspace/venv/bin/python
-# EASY-INSTALL-SCRIPT: 'Django==1.9','django-admin.py'
-__requires__ = 'Django==1.9'
-__import__('pkg_resources').run_script('Django==1.9', 'django-admin.py')
